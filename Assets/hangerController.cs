4ee95f13d782b9c66fe33e7370f877ffcfe370c3
Ruben Zhang <benzhangq@gmail.com>
2017-07-25 23:42:18 -0700

    scroll view integration
    added nested scroll view prefab and figured out how it works

--- Assets/hangerController.cs
+++ Assets/hangerController.cs
@@ -8,12 +8,15 @@ public class hangerController : MonoBehaviour {
     public Transform secondaryPanel;
     public Transform tertiaryPanel;
 
-    public Transform activePanel;
+    private Transform activePanel;
 
     public GameObject buttonPrefab;
+    public GameObject innerPanelPrefab;
 
     public int minimizedPanelSize = 50;
     public int maximizedPanelSize = 500;
+    public float innerPanelXScale = 0.9f;
+    public float innerPanelYScale = 0.9f;
 
     public string dynamicTag = "dynamicUI";
 
@@ -41,7 +44,7 @@ public class hangerController : MonoBehaviour {
             rootPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
             cleanPanel(activePanel);
             this.activePanel = rootPanel;
-            spawnPanelButtons(rootPanel, panelFunc.first);
+            spawnDynamicPanel(rootPanel, panelFunc.first);
         }
         else
         {
@@ -61,7 +64,7 @@ public class hangerController : MonoBehaviour {
             secondaryPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
             cleanPanel(activePanel);
             this.activePanel = secondaryPanel;
-            spawnPanelButtons(secondaryPanel, panelFunc.second);
+            spawnDynamicPanel(secondaryPanel, panelFunc.second);
         }
         else
         {
@@ -81,7 +84,7 @@ public class hangerController : MonoBehaviour {
             tertiaryPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
             cleanPanel(activePanel);
             this.activePanel = tertiaryPanel;
-            spawnPanelButtons(tertiaryPanel, panelFunc.second);
+            spawnDynamicPanel(tertiaryPanel, panelFunc.second);
         }
         else
         {
@@ -89,15 +92,44 @@ public class hangerController : MonoBehaviour {
         }
     }
 
-    private void spawnPanelButtons(Transform activeP, panelFunc func)
+    public void scroll()
     {
+       // activePanel
+    }
+
+    private void spawnDynamicPanel(Transform activeP, panelFunc func)
+    {
+        GameObject newInnerPanel = (GameObject)Instantiate(innerPanelPrefab);
+        newInnerPanel.transform.SetParent(activeP);
+        newInnerPanel.transform.localScale = new Vector3(innerPanelXScale, innerPanelYScale, 1);
+
+        RectTransform innerPanelRect = newInnerPanel.GetComponent<RectTransform>();
+        innerPanelRect.anchorMin = new Vector2(0.5f, 0.5f);
+        innerPanelRect.anchorMax = new Vector2(0.5f, 0.5f);
+        innerPanelRect.anchoredPosition = new Vector2(0, 0);
+        innerPanelRect.sizeDelta = new Vector2(maximizedPanelSize * innerPanelXScale, 500);
+        innerPanelRect.tag = dynamicTag;
+
+        RectTransform viewport = (RectTransform) innerPanelRect.Find("Viewport");
+        viewport.anchorMin = new Vector2(0, 0);
+        viewport.anchorMax = new Vector2(1, 1);
+        viewport.anchoredPosition = new Vector2(0, 0);
+
+        RectTransform contentPanel = (RectTransform) viewport.Find("Content");
+        contentPanel.anchorMin = new Vector2(0, 0);
+        contentPanel.anchorMax = new Vector2(1, 1);
+        contentPanel.anchoredPosition = new Vector2(0, 0);
+
         GameObject newButton = (GameObject)Instantiate(buttonPrefab);
-        newButton.transform.SetParent(activeP);
+        newButton.transform.SetParent(contentPanel);
         newButton.transform.localScale = new Vector3(1, 1, 1);
-        newButton.transform.localPosition = new Vector3(maximizedPanelSize / 2, 0, 0);
+
+        RectTransform buttonTransform = newButton.GetComponent<RectTransform>();
+        buttonTransform.anchorMin = new Vector2(0.45f, 0.5f);
+        buttonTransform.anchorMax = new Vector2(0.45f, 0.5f);
+        buttonTransform.anchoredPosition = new Vector2(0, 0);
 
         Button buttonComponent = newButton.GetComponent<Button>();
-        buttonComponent.tag = dynamicTag;
         buttonComponent.onClick.AddListener(() => testclick());
     }
 
@@ -105,10 +137,15 @@ public class hangerController : MonoBehaviour {
     {
         if (activeP == null)
             return;
-        foreach (Button b in activeP.GetComponentsInChildren<Button>())
+        /*foreach (Button b in activeP.GetComponentsInChildren<Button>())
         {
             if (b.tag.Equals(dynamicTag))
                 GameObject.DestroyObject(b.gameObject);
+        }*/
+        foreach (RectTransform r in activeP.GetComponentsInChildren<RectTransform>())
+        {
+            if (r.tag.Equals(dynamicTag))
+                GameObject.DestroyObject(r.gameObject);
         }
     }
 
