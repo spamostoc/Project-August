07ac8bacc8a26303cf241a195a4787a617740396
Ruben Zhang <benzhangq@gmail.com>
2017-07-27 22:38:24 -0700

    changed hanger ui to be statically allocated
    made hanger ui statically allocated

---
+++ Assets/ArrayList.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+
+internal class ArrayList<T> : List<Button>
+{
+}
--- Assets/hangerController.cs
+++ Assets/hangerController.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class hangerController : MonoBehaviour {
 
     public Transform rootPanel;
-    public Transform secondaryPanel;
-    public Transform tertiaryPanel;
+    private ScrollPanel rootScroll;
+    public Transform secondPanel;
+    private ScrollPanel secondScroll;
+    public Transform thirdPanel;
+    private ScrollPanel thirdScroll;
 
     private Transform activePanel;
 
     public GameObject buttonPrefab;
     public GameObject innerPanelPrefab;
 
-    public int minimizedPanelSize = 50;
-    public int maximizedPanelSize = 500;
-    public float innerPanelXScale = 0.9f;
-    public float innerPanelYScale = 0.9f;
+    public static int minimizedPanelSize = 50;
+    public static int maximizedPanelSize = 500;
+    public static float innerPanelXScale = 0.8f;
+    public static float innerPanelYScale = 0.8f;
 
     public string dynamicTag = "dynamicUI";
 
+
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        rootScroll = new ScrollPanel(rootPanel);
+        rootScroll.scrollPanel.gameObject.SetActive(false);
+        secondScroll = new ScrollPanel(secondPanel);
+        secondScroll.scrollPanel.gameObject.SetActive(false);
+        thirdScroll = new ScrollPanel(thirdPanel);
+        thirdScroll.scrollPanel.gameObject.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -37,14 +48,15 @@ public class hangerController : MonoBehaviour {
         if (activePanel != null)
         {
             activePanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(minimizedPanelSize, 0);
-            cleanPanel(rootPanel);
+            rootScroll.scrollPanel.gameObject.SetActive(false);
         }
         if (activePanel != rootPanel)
         {
             rootPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
-            cleanPanel(activePanel);
             this.activePanel = rootPanel;
-            spawnDynamicPanel(rootPanel, panelFunc.first);
+            rootScroll.scrollPanel.gameObject.SetActive(true);
+            secondScroll.scrollPanel.gameObject.SetActive(false);
+            thirdScroll.scrollPanel.gameObject.SetActive(false);
         }
         else
         {
@@ -57,14 +69,15 @@ public class hangerController : MonoBehaviour {
         if (activePanel != null)
         {
             activePanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(minimizedPanelSize, 0);
-            cleanPanel(secondaryPanel);
+            secondScroll.scrollPanel.gameObject.SetActive(false);
         }
-        if (activePanel != secondaryPanel)
+        if (activePanel != secondPanel)
         {
-            secondaryPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
-            cleanPanel(activePanel);
-            this.activePanel = secondaryPanel;
-            spawnDynamicPanel(secondaryPanel, panelFunc.second);
+            secondPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
+            this.activePanel = secondPanel;
+            rootScroll.scrollPanel.gameObject.SetActive(false);
+            secondScroll.scrollPanel.gameObject.SetActive(true);
+            thirdScroll.scrollPanel.gameObject.SetActive(false);
         }
         else
         {
@@ -77,14 +90,15 @@ public class hangerController : MonoBehaviour {
         if (activePanel != null)
         {
             activePanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(minimizedPanelSize, 0);
-            cleanPanel(tertiaryPanel);
+            thirdScroll.scrollPanel.gameObject.SetActive(false);
         }
-        if (activePanel != tertiaryPanel)
+        if (activePanel != thirdPanel)
         {
-            tertiaryPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
-            cleanPanel(activePanel);
-            this.activePanel = tertiaryPanel;
-            spawnDynamicPanel(tertiaryPanel, panelFunc.second);
+            thirdPanel.GetComponentInChildren<RectTransform>().sizeDelta = new Vector2(maximizedPanelSize, 0);
+            this.activePanel = thirdPanel;
+            rootScroll.scrollPanel.gameObject.SetActive(false);
+            secondScroll.scrollPanel.gameObject.SetActive(false);
+            thirdScroll.scrollPanel.gameObject.SetActive(true);
         }
         else
         {
@@ -153,4 +167,39 @@ public class hangerController : MonoBehaviour {
     {
         Debug.Log("button is clicked");
     }
+
+    class ScrollPanel
+    {
+        public string dynamicTag = "dynamicUI";
+
+        public RectTransform scrollPanel;
+        public RectTransform viewport;
+        public RectTransform contentPanel;
+        public List<Button> buttonList;
+
+        public ScrollPanel(Transform basePanel)
+        {
+            RectTransform newInnerPanel = (RectTransform)basePanel.Find("Scroll Panel");
+
+            scrollPanel = newInnerPanel.GetComponent<RectTransform>();
+            scrollPanel.anchorMin = new Vector2(0.45f, 0.5f);
+            scrollPanel.anchorMax = new Vector2(0.45f, 0.5f);
+            scrollPanel.anchoredPosition = new Vector2(0, 0);
+            scrollPanel.sizeDelta = new Vector2(maximizedPanelSize * innerPanelXScale, 500);
+            scrollPanel.tag = dynamicTag;
+
+            viewport = (RectTransform)scrollPanel.Find("Viewport");
+            viewport.anchorMin = new Vector2(0, 0);
+            viewport.anchorMax = new Vector2(1, 1);
+            viewport.anchoredPosition = new Vector2(0, 0);
+
+            contentPanel = (RectTransform)viewport.Find("Content");
+            contentPanel.anchorMin = new Vector2(0, 0);
+            contentPanel.anchorMax = new Vector2(1, 1);
+            contentPanel.anchoredPosition = new Vector2(0, 0);
+
+            buttonList = new ArrayList<Button>();
+        }
+    }
 }
+
